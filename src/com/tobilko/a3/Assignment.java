1e4bf04c707c4361ba26bf2a5a03f977c00e2513
Andrew Tobilko <andrew.tobilko@gmail.com>
2018-02-22 01:13:51 +0200

    a3

--- src/com/tobilko/a3/Assignment.java
+++ src/com/tobilko/a3/Assignment.java
@@ -1,8 +1,20 @@
 package com.tobilko.a3;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Assignment {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        final String url = "jdbc:Cache://127.0.0.1:1972/SAMPLES";
+        final String user = "Andrew";
+        final String password = "Tobilko";
+
+        final Connection connection = DriverManager.getConnection(url, user, password);
+
+
+
 
     }
 
