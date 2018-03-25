bc42cbf2c89c36baf01bc14b5b9d1bd9ca80b6c8
Andrew Tobilko <andrew.tobilko@gmail.com>
2018-03-25 23:17:35 +0300

    all

--- src/com/tobilko/a3/Assignment.java
+++ src/com/tobilko/a3/Assignment.java
@@ -1,20 +1,68 @@
 package com.tobilko.a3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.intersys.globals.Connection;
+import com.intersys.globals.ConnectionContext;
+import com.intersys.globals.NodeReference;
 
+import java.util.Scanner;
+
+
+/**
+ * Для глобалу з л/р №2 розробити програму на мові високого рівня
+ * для перегляду та зміни значень вузлів та індексів глобала.
+ */
 public class Assignment {
 
-    public static void main(String[] args) throws SQLException {
-        final String url = "jdbc:Cache://127.0.0.1:1972/SAMPLES";
+    private static final NodeReference reference;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    static {
+        final String namespace = "ANIMALS";
         final String user = "Andrew";
         final String password = "Tobilko";
+        final String globalName = "Animals";
+        final Connection connection;
 
-        final Connection connection = DriverManager.getConnection(url, user, password);
+        (connection = ConnectionContext.getConnection()).connect(namespace, user, password);
+        reference = connection.createNodeReference(globalName);
 
+        Runtime.getRuntime().addShutdownHook(new Thread(connection::close));
+    }
 
+    public static void main(String[] args) {
+        boolean restartSession;
+        do {
+            System.out.println("Do you want to get [0] or edit [1] the global?");
+            int action = Integer.valueOf(scanner.nextLine());
 
+            if (action != 0 && action != 1) {
+                System.out.println("Incorrect choice. I hope you'll be more accurate in typing next time.");
+            } else {
+                startSession(action);
+            }
+
+            System.out.println("The session is over. Would you like to start a new session [true] or close the app [false]?");
+            restartSession = Boolean.valueOf(scanner.nextLine());
+        } while (restartSession);
+    }
+
+    private static void startSession(int type) {
+        final String referenceName = reference.getName();
+
+//        if (type == 0) {
+//            System.out.println(String.format("Fetching data for '%s'...", referenceName));
+//
+//            System.out.println(reference.getObject("ages"));
+//            System.out.println(reference.getString("aliases"));
+//            System.out.println(reference.getList("names"));
+//            System.out.println(reference.getString("title"));
+//            System.out.println(reference.getInt("number"));
+//¬≤
+//        } else {
+//            System.out.println("Which --- do you want to edit?");
+//            final String s = scanner.nextLine();
+//
+//        }
 
     }
 
